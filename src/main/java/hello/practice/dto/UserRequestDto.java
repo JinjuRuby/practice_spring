89eca8e15adefe633a47cc9dee73b15fd3022e76
JinjuRuby <ronnie0304@naver.com>
2025-12-16 18:47:24 +0900

    refactor: DTO 수동 생성자 제거 및 롬복 어노테이션 적용
    - BoardRequestDto, UserRequestDto 수정
    - 직접 작성한 생성자를 삭제하고 @AllArgsConstructor로 대체
    - 메서드 레벨의 @Builder를 클래스 레벨로 변경하여 가독성 및 유지보수성 향상

--- src/main/java/hello/practice/dto/UserRequestDto.java
+++ src/main/java/hello/practice/dto/UserRequestDto.java
@@ -2,20 +2,15 @@ package hello.practice.dto;
 
 import lombok.*;
 
-// 회원가입을 할 때 사용하는 DTO
+// 회원가입을 할 때 사용자에게 정보를 받는 DTO
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class UserRequestDto {
 
     private String userName;
     private String email;
     private String password;
 
-    @Builder
-    public UserRequestDto(String userName, String email, String password) {
-        this.userName = userName;
-        this.email = email;
-        this.password = password;
-    }
 }
